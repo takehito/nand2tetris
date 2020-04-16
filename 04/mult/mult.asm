045be919224e8047ea66622276fc4c8c49e60782
takehito <utisama.produce@gmail.com>
2020-04-17 03:31:43 +0900

    4/17の分

---
+++ 04/mult/mult.asm
@@ -0,0 +1,29 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/04/Mult.asm
+
+// Multiplies R0 and R1 and stores the result in R2.
+// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+
+// Put your code here.
+@R2
+M=0
+@i
+M=0
+(Loop)
+	@i
+	D=M
+	@R1
+	D=D-M
+	@END
+	D;JGE
+	@R0
+	D=M
+	@R2
+	M=M+D
+	@i
+	M=M+1
+	@Loop
+	0;JMP
+(END)
